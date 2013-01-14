a4b68fe63383461aa9e3cc3f8cbdea5cbecccfb5
russ <russ@russ-Satellite-L305.(none)>
2013-01-14 09:29:49 -0500

    Added some source added auth.json to .gitignore

--- src/tweet/Post.java
+++ src/tweet/Post.java
@@ -1,5 +1,141 @@
 package tweet;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
+import twitter4j.Twitter;
+import twitter4j.TwitterException;
+import twitter4j.TwitterFactory;
+import twitter4j.auth.AccessToken;
+import twitter4j.auth.RequestToken;
+
 public class Post {
-   
+
+	public final static String CONSUMER_KEY = "yNBLlBrsFHz89PyCfjrAw";
+	public final static String CONSUMER_KEY_SECRET = "8SIq5OXfeIKabtB3B2CBHJVIkrjQbSPloHoTmxtis4";
+
+	public void start() {
+		Twitter twitter = new TwitterFactory().getInstance();
+		twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_KEY_SECRET);
+
+		try {
+			twitter.setOAuthAccessToken(getOathAccessToken(twitter));
+		} catch (TwitterException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		try {
+			BufferedReader br = new BufferedReader(new InputStreamReader(
+					System.in));
+			System.out.print("Status update: ");
+			twitter.updateStatus(br.readLine());
+
+		} catch (TwitterException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	private AccessToken getOathAccessToken(Twitter twitter)
+			throws TwitterException {
+		String token = getAccessToken();
+		String tokenSecret = getAccessTokenSecret();
+		if (token == null || tokenSecret == null) {
+			return setAccessToken(twitter);
+		}
+		return new AccessToken(token, tokenSecret);
+	}
+
+	private AccessToken setAccessToken(Twitter twitter) throws TwitterException {
+		RequestToken requestToken = twitter.getOAuthRequestToken();
+		System.out.println("Authorization URL: \n"
+				+ requestToken.getAuthorizationURL());
+
+		AccessToken accessToken = null;
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		while (null == accessToken) {
+			try {
+				System.out.print("Input PIN here: ");
+				String pin = br.readLine();
+
+				accessToken = twitter.getOAuthAccessToken(requestToken, pin);
+
+			} catch (TwitterException te) {
+
+				System.out.println("Failed to get access token, caused by: "
+						+ te.getMessage());
+
+				System.out.println("Retry input PIN");
+
+			} catch (IOException e) {
+				System.out.println("Retry input PIN");
+			}
+		}
+
+		addToFile(accessToken);
+
+		return accessToken;
+	}
+
+	private void addToFile(AccessToken accessToken) {
+		JSONObject obj = new JSONObject();
+		obj.put("AccessToken", accessToken.getToken());
+		obj.put("AccessTokenSecret", accessToken.getTokenSecret());
+
+		try {
+			FileWriter file = new FileWriter("./auth.json");
+			file.write(obj.toJSONString());
+			file.flush();
+			file.close();
+
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+	}
+
+	public String getAccessToken() {
+		JSONParser parser = new JSONParser();
+		try {
+
+			Object obj = parser.parse(new FileReader("./auth.json"));
+
+			JSONObject jsonObject = (JSONObject) obj;
+
+			return (String) jsonObject.get("AccessToken");
+
+		} catch (Throwable e) {
+			return null;
+		}
+	}
+
+	public String getAccessTokenSecret() {
+		JSONParser parser = new JSONParser();
+		try {
+
+			Object obj = parser.parse(new FileReader("./auth.json"));
+
+			JSONObject jsonObject = (JSONObject) obj;
+
+			return (String) jsonObject.get("AccessTokenSecret");
+
+		} catch (Throwable e) {
+			return null;
+		}
+	}
+
+	public static void main(String[] args) {
+		new Post().start();
+
+	}
 }
--- src/tweet/Tweet.java
+++
@@ -1,9 +0,0 @@
-package tweet;
-
-public class Tweet {
-	public static void main(String[] args) {
-		System.out.println("hello world");
-		//added a comment 
-	}
-}
-
