251f6e8e528f8538f84fff347ab5a8077d80931a
Vincenzo Pavano <vincenzopavano@gmail.com>
2013-02-01 09:11:46 -0500

    Created MCV packages, started building the GUI. Created the frame, JMenuBar, and JTabbedPane.

--- src/tweet/Info.java
+++
@@ -1,9 +0,0 @@
-package tweet;
-
-public class Info {
-
-	// apps consumer key and consumer key secret
-	public final static String CONSUMER_KEY = "yNBLlBrsFHz89PyCfjrAw";
-	public final static String CONSUMER_KEY_SECRET = "8SIq5OXfeIKabtB3B2CBHJVIkrjQbSPloHoTmxtis4";
-
-}
