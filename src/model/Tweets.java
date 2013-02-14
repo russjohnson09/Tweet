7096b35362357b5510e998156a5f43967b78c023
russjohnson09 <russjohnson09@gmail.com>
2013-02-14 16:22:30 -0500

    removed checkstyle errors from Tweets.java

--- src/model/Tweets.java
+++ src/model/Tweets.java
@@ -12,60 +12,64 @@ import twitter4j.Status;
  ***************************************************/
 public class Tweets extends AbstractListModel<String> {
 
-	/** serialVersionUID. */
-	private static final long serialVersionUID = 1L;
+    /** serialVersionUID. */
+    private static final long serialVersionUID = 1L;
 
-	/** ArrayList tweets. */
-	private ArrayList<Status> tweets;
+    /** ArrayList tweets. */
+    private ArrayList<Status> tweets;
 
-	/****************************************************
-	 * Tweets Constructor.
-	 ***************************************************/
-	public Tweets() {
-		super();
-		tweets = new ArrayList<Status>();
-	}
+    /****************************************************
+     * Tweets Constructor.
+     ***************************************************/
+    public Tweets() {
+        super();
+        tweets = new ArrayList<Status>();
+    }
 
-	@Override
-	public final String getElementAt(final int i) {
-		Status s = tweets.get(i);
+    @Override
+    public final String getElementAt(final int i) {
+        Status s = tweets.get(i);
 
-		SimpleDateFormat df = new SimpleDateFormat();
-		df.applyPattern("MM/dd/yyyy");
+        SimpleDateFormat df = new SimpleDateFormat();
+        df.applyPattern("MM/dd/yyyy");
 
-		String str = "Time: ";
-		str += df.format(s.getCreatedAt());
-		str += ", Status: ";
-		str += s.getText();
+        String str = "Time: ";
+        str += df.format(s.getCreatedAt());
+        str += ", Status: ";
+        str += s.getText();
 
-		return str;
-	}
+        return str;
+    }
 
-	@Override
-	/****************************************************
-	 * Gets size of tweet.
-	 ***************************************************/
-	public final int getSize() {
-		return tweets.size();
-	}
+    @Override
+    /****************************************************
+     * Gets size of tweet.
+     ***************************************************/
+    public final int getSize() {
+        return tweets.size();
+    }
 
-	/****************************************************
-	 * Adds tweet.
-	 * @param s Status
-	 ***************************************************/
-	public final void add(final Status s) {
-		tweets.add(s);
-	}
+    /****************************************************
+     * Adds tweet.
+     *
+     * @param s
+     *            Status
+     ***************************************************/
+    public final void add(final Status s) {
+        tweets.add(s);
+    }
 
-	/****************************************************
-	 * Removes Tweet.
-	 * @param index int
-	 * @return l long
-	 ***************************************************/
-	public final long remove(final int index) {
-		long l = tweets.get(index).getId();
-		tweets.remove(index);
-		fireIntervalRemoved(this, 0, tweets.size());
-		return l;
-	}
+    /****************************************************
+     * Removes Tweet.
+     *
+     * @param index
+     *            int
+     * @return l long
+     ***************************************************/
+    public final long remove(final int index) {
+        long l = tweets.get(index).getId();
+        tweets.remove(index);
+        fireIntervalRemoved(this, 0, tweets.size());
+        return l;
+    }
 }
